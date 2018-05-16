509e86236ef01596360c6cf1cc9cb0431b1a13ca
Danny Cheng <danny.cheng@kinja.com>
2018-05-16 15:52:22 -0500

    Return the type of socialReferrer instead of just returning true or false.

--- src/helpers/SocialReferrer.js
+++ src/helpers/SocialReferrer.js
@@ -4,18 +4,25 @@ var SocialReferrer = {
    *
    * @returns {String};
    */
-  getReferrer: function() {
+  getReferrer: function () {
     return window.document.referrer || '';
   },
  /**
-  * Determines if the referrer is social media
+  * Determines if the referrer is social media.
+  * If true, we want to return the type of social media
   *
   * @returns {Boolean};
   */
-  isSocialReferrer: function() {
-    var socialReferrer = this.getReferrer().match(/\b(?:facebook|instagram|pinterest|reddit|twitter|tumblr|t\.co)\b/i);
+  isSocialReferrer: function () {
+    // brandMap is used to match when it is a twitter short url
+    var socialReferrer = this.getReferrer().match(/\b(?:facebook|instagram|pinterest|reddit|twitter|tumblr|t\.co)\b/i),
+      brandMap = {
+        't.co': 'twitter'
+      };
     if (socialReferrer) {
-      return true;
+      // if the match is positive, return the first result and check if it is a twitter short url
+      socialReferrer = socialReferrer[0].toLowerCase();
+      return brandMap[socialReferrer] || socialReferrer;
     }
     return false;
   }
