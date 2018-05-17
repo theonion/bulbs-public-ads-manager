0c187397bb9960fbd0b3be93aac4403ce6b7406c
Danny Cheng <danny.cheng@kinja.com>
2018-05-17 09:39:26 -0500

    Update tests to expect strings instead of true/false.

--- src/helpers/SocialReferrer.spec.js
+++ src/helpers/SocialReferrer.spec.js
@@ -1,39 +1,39 @@
-describe('SocialReferrer', function() {
+describe('SocialReferrer', function () {
   var SocialReferrer;
 
-  beforeEach(function() {
+  beforeEach(function () {
     SocialReferrer = require('./SocialReferrer');
   });
 
-  describe('#isSocialReferrer', function() {
-    describe('test for facebook a social referrer', function() {
-      beforeEach(function() {
+  describe('#getSocialReferrer', function () {
+    describe('test for facebook a social referrer', function () {
+      beforeEach(function () {
         TestHelper.stub(SocialReferrer, 'getReferrer').returns('facebook.com');
       });
 
-      it('this referrer should be a social one', function() {
-        expect(SocialReferrer.isSocialReferrer()).to.equal('facebook');
+      it('this referrer should be a social one', function () {
+        expect(SocialReferrer.getSocialReferrer()).to.equal('facebook');
       });
     });
 
-    describe('test for instagram as a social referrer', function() {
-      beforeEach(function() {
-          TestHelper.stub(SocialReferrer, 'getReferrer').returns('instagram.com');
+    describe('test for instagram as a social referrer', function () {
+      beforeEach(function () {
+        TestHelper.stub(SocialReferrer, 'getReferrer').returns('instagram.com');
 
       });
 
-      it('this referrer should be a social one', function() {
-        expect(SocialReferrer.isSocialReferrer()).to.equal('instagram');
+      it('this referrer should be a social one', function () {
+        expect(SocialReferrer.getSocialReferrer()).to.equal('instagram');
       });
     });
 
-    describe('test for a non-social referrer', function() {
-        beforeEach(function() {
-          TestHelper.stub(SocialReferrer, 'getReferrer').returns('notathing.com');
-        });
+    describe('test for a non-social referrer', function () {
+      beforeEach(function () {
+        TestHelper.stub(SocialReferrer, 'getReferrer').returns('notathing.com');
+      });
 
-      it('this referrer should not be a social one', function() {
-        expect(SocialReferrer.isSocialReferrer()).to.be.false;
+      it('this referrer should not be a social one', function () {
+        expect(SocialReferrer.getSocialReferrer()).to.equal('');
       });
     });
 
